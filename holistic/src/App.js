8625adb74865695a46f068922d67ed5bbc60ff0a
Ankesh Kumar <ankesh9053@gmail.com>
2023-02-23 10:56:18 +0530

    Day2

--- holistic/src/App.js
+++ holistic/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "../src/Components/Navbar"
+import Homepage from './Components/Homepage';
 
 function App() {
   return (
+    
     <div className="App">
-    <h1>Hello world</h1>
+   <Homepage/>
     </div>
   );
 }
